b4e4f95559baf006898c0f3c0781e55b949ae257
yudai <yudai201729@icloud.com>
2022-08-30 06:49:39 +0900

    👍日程とチャンネルメッセージ取得

--- src/slackGetMsg.py
+++ src/slackGetMsg.py
@@ -1,5 +1,7 @@
+import datetime
 import requests
 import settings
+import pandas as pd
 
 SLACK_CHANNEL_ID = settings.SLACK_CHANNEL_ID
 SLACK_URL = settings.SLACK_URL
@@ -9,17 +11,36 @@ def main():
 
     payload = {
         "channel": SLACK_CHANNEL_ID,
-        "oldest": "1622761200"
+        "limit":"1000"
     }
     headersAuth = {
     'Authorization': 'Bearer '+ str(TOKEN),
-    }  
+    }
     response = requests.get(SLACK_URL, headers=headersAuth, params=payload)
     json_data = response.json()
     msgs = json_data['messages']
+
+    text_list = []
+    time_list = []
+    list = []
     for msg in msgs:
-        print(msg['text'])
-    return 
+        try:
+            text = msg["text"]
+            ts = float(msg["ts"])
+            text_list.append(text)
+            dt = datetime.datetime.fromtimestamp(ts)
+            s = dt.strftime("%Y-%m-%d %H:%M:%S")
+            time_list.append(s)
+            list.append([text,s])
+        except KeyError:
+            pass
+
+    df = pd.DataFrame(list,columns=['テキスト', '日付'])
+    df.to_excel('記事録.xlsx')
+    print(df)
+
+
+    return
 
 if __name__ == "__main__":
     main()

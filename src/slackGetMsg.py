6e2510c5ffbb11b95ccacc173e2106432f1b5c02
yudai <yudai201729@icloud.com>
2022-09-03 11:55:44 +0900

    👍エクセルに吐き出せるように変更

--- src/slackGetMsg.py
+++ src/slackGetMsg.py
@@ -7,14 +7,15 @@ SLACK_CHANNEL_ID = settings.SLACK_CHANNEL_ID
 SLACK_URL = settings.SLACK_URL
 TOKEN = settings.TOKEN
 
+
 def main():
 
     payload = {
         "channel": SLACK_CHANNEL_ID,
-        "limit":"1000"
+        "limit": "1000"
     }
     headersAuth = {
-    'Authorization': 'Bearer '+ str(TOKEN),
+        'Authorization': 'Bearer ' + str(TOKEN),
     }
     response = requests.get(SLACK_URL, headers=headersAuth, params=payload)
     json_data = response.json()
@@ -31,16 +32,16 @@ def main():
             dt = datetime.datetime.fromtimestamp(ts)
             s = dt.strftime("%Y-%m-%d %H:%M:%S")
             time_list.append(s)
-            list.append([text,s])
+            list.append([text, s])
         except KeyError:
             pass
 
-    df = pd.DataFrame(list,columns=['テキスト', '日付'])
-    df.to_excel('記事録.xlsx')
+    df = pd.DataFrame(list, columns=['テキスト', '日付'])
+    df.to_excel('xxxxx.xlsx')
     print(df)
 
-
     return
+
 
 if __name__ == "__main__":
     main()
